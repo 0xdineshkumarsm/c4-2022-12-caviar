074aab5259f4e32a008b915523b2831bb0c694d5
0xdineshkumarsm <105421120+0xdineshkumarsm@users.noreply.github.com>
2022-12-19 22:15:51 +0530

    test 4 excess token refund

--- test/Pair/integration/AddBuySellRemove.t.sol
+++ test/Pair/integration/AddBuySellRemove.t.sol
@@ -43,4 +43,62 @@ contract AddBuySellRemoveTest is Fixture {
             "Should have removed correct fractional token amount"
         );
     }
+
+        function testItAddsRemovesCorrectAmount() public {
+        console.log("Base:fractional:LP ",type(uint96).max);
+                // add liquidity
+        uint initBaseTokenAmount = 1000e18;
+        uint initFractionalTokenAmount = 10e18;
+        deal(address(usd), address(this), initBaseTokenAmount, true);
+        deal(address(p), address(this), initFractionalTokenAmount, true);
+        uint256 minLpTokenAmount = Math.sqrt(initBaseTokenAmount * initFractionalTokenAmount);
+        usd.approve(address(p), type(uint256).max);
+        uint initlpTokenAmount = p.add(initBaseTokenAmount, initFractionalTokenAmount, minLpTokenAmount);
+
+        console.log("initated Pool ",initBaseTokenAmount,initFractionalTokenAmount,initlpTokenAmount);
+        console.log("Pool Info ->  ",usd.balanceOf(address(p)),p.balanceOf(address(p)),lpToken.totalSupply());
+
+        console.log("-------------------------------------------------------");
+
+        uint addBaseTokenAmount = 300e18;
+        uint addFractionalTokenAmount = 1e18;
+        deal(address(usd), babe, addBaseTokenAmount, true);
+        deal(address(p), babe, addFractionalTokenAmount, true);
+
+        vm.startPrank(babe);
+        usd.approve(address(p), type(uint256).max);
+
+
+        console.log("adding ",addBaseTokenAmount,addFractionalTokenAmount);
+
+        minLpTokenAmount = p.addQuote(addBaseTokenAmount,addFractionalTokenAmount);
+        uint addedlpTokenAmount = p.add(addBaseTokenAmount, addFractionalTokenAmount, minLpTokenAmount);
+        console.log("----------------------- after Added ---------------------");
+        // console.log(lpToken.totalSupply(),addBaseTokenAmount,addFractionalTokenAmount);
+        console.log("Pool Info ->  ",usd.balanceOf(address(p)),p.balanceOf(address(p)),lpToken.totalSupply());
+        console.log("babe's balance ",usd.balanceOf(address(babe)),p.balanceOf(address(babe)),lpToken.balanceOf(address(babe)));
+
+
+        console.log("----------------------- after Removed ---------------------");
+
+        (uint a,uint b) = p.removeQuote(addedlpTokenAmount);
+        p.remove(addedlpTokenAmount,a,b);
+
+        console.log("Pool Info ->  ",usd.balanceOf(address(p)),p.balanceOf(address(p)),lpToken.totalSupply());
+        console.log("babe's balance ",usd.balanceOf(address(babe)),p.balanceOf(address(babe)),lpToken.balanceOf(address(babe)));
+
+
+        // since there is no swaps,removing will have to return same amt of token deposited
+
+        uint baseTokenBalAfterRemoval = usd.balanceOf(address(babe));
+
+        assertEq(addBaseTokenAmount,baseTokenBalAfterRemoval,"must return same amt deposited since no swaps");
+
+        console.log("diff -> ",( addBaseTokenAmount - baseTokenBalAfterRemoval)/1e18,( addBaseTokenAmount - baseTokenBalAfterRemoval)%1e18);
+
+
+
+        vm.stopPrank();
+
+    }
 }
